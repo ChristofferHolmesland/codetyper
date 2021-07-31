6f1fa036acf95e5138677b20e1e88250b8c4681b
Tharun Viswanathan <tharun.tvishy@gmail.com>
2021-07-31 13:53:47 +0530

    Fixing formatting a bit

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -119,61 +119,61 @@ function callFunc() {
 var lang = "";
 java.addEventListener("click", () => {
 	myCode = myCodeArr[1];
-        document.getElementById("langDiv").innerHTML = "" + "Java";
+        document.getElementById("langDiv").innerHTML = "Java";
 	callFunc();
 });
 
 c.addEventListener("click", () => {
 	myCode = myCodeArr[2];
-        document.getElementById("langDiv").innerHTML = "" + "C";
+        document.getElementById("langDiv").innerHTML = "C";
 	callFunc();
 });
 
 cpp.addEventListener("click", () => {
 	myCode = myCodeArr[3];
-        document.getElementById("langDiv").innerHTML = "" + "C++";
+        document.getElementById("langDiv").innerHTML = "C++";
         lang = "C";
 	callFunc();
 });
 
 csharp.addEventListener("click", () => {
 	myCode = myCodeArr[4];
-        document.getElementById("langDiv").innerHTML = "" + "C#";
+        document.getElementById("langDiv").innerHTML = "C#";
 	callFunc();
 });
 
 py.addEventListener("click", () => {
 	myCode = myCodeArr[0];
-        document.getElementById("langDiv").innerHTML = "" + "Python";
+        document.getElementById("langDiv").innerHTML = "Python";
 	callFunc();
 });
 
 go.addEventListener("click", () => {
 	myCode = myCodeArr[5];
-        document.getElementById("langDiv").innerHTML = "" + "Golang";
+        document.getElementById("langDiv").innerHTML = "Golang";
 	callFunc();
 });
 
 kotlin.addEventListener("click", () => {
 	myCode = myCodeArr[6];
-        document.getElementById("langDiv").innerHTML = "" + "Kotlin";
+        document.getElementById("langDiv").innerHTML = "Kotlin";
 	callFunc();
 });
 
 js.addEventListener("click", () => {
 	myCode = myCodeArr[7];
-        document.getElementById("langDiv").innerHTML = "" + "Javascript";
+        document.getElementById("langDiv").innerHTML = "Javascript";
 	callFunc();
 });
 
 bash.addEventListener("click", () => {
 	myCode = myCodeArr[8];
-        document.getElementById("langDiv").innerHTML = "" + "Bash";
+        document.getElementById("langDiv").innerHTML = "Bash";
 	callFunc();
 });
 rust.addEventListener("click", () => {
 	myCode = myCodeArr[9];
-        document.getElementById("langDiv").innerHTML = "" + "Rust";
+        document.getElementById("langDiv").innerHTML = "Rust";
 	callFunc();
 });
 
