33331260db5bcc659f1facb8301186c525bc528a
Christoffer Holmesland <ChristofferHolmesland@users.noreply.github.com>
2022-07-01 22:29:50 +0200

    Inject javascript in index.html header

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const path = require("path")
 const express = require("express");
 const chokidar = require("chokidar");
@@ -5,7 +6,27 @@ const chokidar = require("chokidar");
 const app = express();
 const port = 3000;
 
+const injectHTML = `
+<script src="/server/inject.js"></script>
+</head>
+`;
+
+app.use("/", (req, res, next) => {
+	if (req.url !== "/") {
+		return next();
+	}
+
+	let content = fs.readFileSync(path.join(__dirname, "../index.html")).toString();
+	content = content.replace("</head>", injectHTML);
+
+	res.status(200).send(content);
+})
+
 app.use(express.static(path.join(__dirname, "..")))
+
+chokidar.watch(path.join(__dirname, "../static/styles/")).on("change", (event, path) => {
+	console.log(event);
+});
 
 app.listen(port, () => {
 	console.log(`CodeTyper dev server listening on ${port}`);
